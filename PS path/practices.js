52bacf5befa779aa31d44d70a7d29931b2d41b51
ebautista <ebautista@encorerepair.com>
2018-05-03 20:52:47 -0400

    Adding get property to object

--- PS path/practices.js
+++ PS path/practices.js
@@ -1,1 +1,32 @@
-alert('Starting....');
+//alert('Starting....');
+console.log('Starting....');
+
+
+// Adding GET property to object
+
+student = {
+	name: "Eudys",
+	lastname: "Bautista",
+	age: "32"
+};
+
+
+console.log('logging student...');
+console.log(student);
+
+
+console.log('Adding GET property');
+
+Object.defineProperty(student, 'fullName', {
+
+	get: function(){
+		return this.name +' '+ this.lastname
+	}
+});
+
+
+console.log('logging student...');
+console.log(student);
+
+console.log('logging fullName student...');
+console.log(student.fullName);
