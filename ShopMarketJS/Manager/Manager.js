a801fe7dbe1726228015dd6ffb97237bcb0f0bab
ebautista <ebautista@encorerepair.com>
2018-05-15 22:21:39 -0400

    Adding new module implementation

--- ShopMarketJS/Manager/Manager.js
+++ ShopMarketJS/Manager/Manager.js
@@ -1,16 +1,21 @@
 function ProductManager() {
 	
 	var products = [];
-	this.product = new Product();
 	
 	this.add =  function(product) {
 		products.push(product);
 	}
 
 	this.edit =  function(product) {
-		var indexItem = getItemIndex(product);
+		var indexItem = getItemIndex(product.id);
 		products[indexItem] = product;
 	};
+
+	this.delete = function(productId) {
+		var indexItem = getItemIndex(productId)
+		products.splice(indexItem,1);
+	}
+
 
 	this.getId = function(id){
 		products.foreach(
@@ -22,15 +27,16 @@ function ProductManager() {
 		);
 	}
 
-	var getItemIndex = function(product){
+	this.getAll = function(){
+		return products;
+	}
+
+	var getItemIndex = function(productId){
 		for (var i  = 0; i < products.lenght; i++) {
-			if (products[i].id == product.id) {
+			if (products[i].id == productId) {
 				return i;
 			}
 		}
 	};
-
-
-
-	//this.delete = 
+	
 }
