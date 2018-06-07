1bbb2fdc52f22657fb0a7d13a2d8a11516d954e4
ebautista <ebautista@encorerepair.com>
2018-06-07 19:14:32 -0400

    Adding new entity proudct using ES6

---
+++ ShopMarketJS/Entity/Product.js
@@ -0,0 +1,23 @@
+class Products{
+
+	construtor(){
+		this.name;
+		this.price;
+	}
+
+	getName(){
+		return this.name;
+	}
+
+	setName(name){
+		this.name = name;
+	}
+
+	getPrice(){
+		return this.price;
+	}
+
+	getPrice(price){
+		this.price = price;
+	}
+}
