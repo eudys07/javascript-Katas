449c6495cf050f698a77e88a0b40ffce2663a20b
ebautista <ebautista@encorerepair.com>
2018-06-10 19:13:40 -0400

    Working With ES6

--- ShopMarketJS/Manager/product-manager.js
+++ ShopMarketJS/Manager/product-manager.js
@@ -2,6 +2,38 @@
 
 class ProductManager{
 	
-	let product = new Product();
+	constructor(){
+		this.product = new Product(); 
+		this.products = [];
+	}
 
+	getAll = () => this.products;
+
+	get = (p) => {
+		for(product of products){
+			if (p === product){
+				return product;
+			}
+		}
+	};	
+
+	add = p => products.push(p); 
+
+	edit = p => {
+		for(let p = 0; p <= products.lenght; p++){
+			if (p.name === products[p].name){
+				products[p].price = p.price;
+			}
+		}		
+	};
+
+	delete= p => {
+		for(let p = 0; p <= products.lenght; p++){
+			if (p.name === products[p].name){
+				delete products[p];
+			}
+		}		
+	};
 }
+
+//console.log(new Product());
