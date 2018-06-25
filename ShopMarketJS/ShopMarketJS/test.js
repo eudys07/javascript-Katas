31e8d3febe7661a2c58b4bd4f2e3d1c78b61e81e
ebautista <ebautista@encorerepair.com>
2018-06-24 23:04:18 -0400

    Adding resources

--- ShopMarketJS/ShopMarketJS/test.js
+++ ShopMarketJS/ShopMarketJS/test.js
@@ -3,3 +3,11 @@
 		console.log('hola');
 	}, 1000)
 	*/
+
+let perks = set(['Eudys', 'Soto']);
+
+console.log(...perks.keys());
+console.log(...perks.values());
+console.log(...perks.entries());
+
+console.log("Ready...");
